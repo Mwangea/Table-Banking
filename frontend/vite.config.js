d41c0cd5a51c7bf605de6d4e8c0544957b302c17
mwangea <mwangeamusa@gmail.com>
2026-02-13 22:46:54 +0300

    Remove local API proxy from Vite configuration and set external API target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:5000',
       '/api': {
         target: 'https://apifinance.hotsportgym.com',
         changeOrigin: true,
